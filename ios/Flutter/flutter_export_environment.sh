fa83c5ae0f707a032cd12a6c2d1e50dae325d794
知音 <45193007+zhiyin520@users.noreply.github.com>
2025-10-22 16:29:38 +0800

    add

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/share/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/64G/zhizhihu-flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/64G/clone/zhizhihu-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Volumes/64G/clone/zhizhihu-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzcuMC0wLjEucHJl,RkxVVFRFUl9DSEFOTkVMPWJldGE=,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NDY1ZTQyMWY1Mw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049Mjg2ZTU5MjE5NA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4wIChidWlsZCAzLjEwLjAtMTYyLjEuYmV0YSk="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Volumes/64G/clone/zhizhihu-flutter/.dart_tool/package_config.json"
